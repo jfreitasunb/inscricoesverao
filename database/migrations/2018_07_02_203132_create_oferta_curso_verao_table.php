126de152c33a99088f8edcc98840a8fb4a81f87e
jfreitasmat <jfreitas@mat.unb.br>
2018-07-02 20:41:09 -0300

    Colunas na tabela de oferta do verão.

--- database/migrations/2018_07_02_203132_create_oferta_curso_verao_table.php
+++ database/migrations/2018_07_02_203132_create_oferta_curso_verao_table.php
@@ -15,6 +15,11 @@ class CreateOfertaCursoVeraoTable extends Migration
     {
         Schema::create('oferta_curso_verao', function (Blueprint $table) {
             $table->increments('id');
+            $table->unsignedInteger('id_inscricao_verao');
+            $table->foreign('id_inscricao_verao')->references('id_inscricao_verao')->on('configura_inscricao_verao')->onDelete('cascade');
+            $table->unsignedInteger('id_curso_verao');
+            $table->foreign('id_curso_verao')->references('id_curso_verao')->on('curso_verao_mat')->onDelete('cascade');
+            $table->boolean('seleciona_pos')->default('0');
             $table->timestamps();
         });
     }
