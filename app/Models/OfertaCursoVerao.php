ed786d41fd1a131fe17daff2a3f559ff4be387fe
jfreitasmat <jfreitas@mat.unb.br>
2018-07-02 20:40:50 -0300

    Tabela da oferta do verão.

--- app/Models/OfertaCursoVerao.php
+++ app/Models/OfertaCursoVerao.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class OfertaCursoVerao extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    
+    protected $primaryKey = 'id';
+
+    protected $table = 'oferta_curso_verao';
+
 }
