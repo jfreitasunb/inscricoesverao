c01313fccb30d73fb73c720020acb5ed7bf6883a
jfreitasmat <jfreitas@mat.unb.br>
2018-07-03 17:29:50 -0300

    Correção no nome de variável.

--- app/Models/Documento.php
+++ app/Models/Documento.php
@@ -23,22 +23,22 @@ class Documento extends Model
     	return $this->select('nome_arquivo')->where('id_candidato',$id_candidato)->orderBy('created_at','desc')->first();
     }
 
-    public function retorna_historico($id_candidato,$id_inscricao_pos)
+    public function retorna_historico($id_candidato,$id_inscricao_verao)
     {
-        if (!is_null($this->select('nome_arquivo')->where('id_candidato',$id_candidato)->where('tipo_arquivo','Histórico')->where('id_inscricao_pos',$id_inscricao_pos)->orderBy('created_at','desc')->first())) {
+        if (!is_null($this->select('nome_arquivo')->where('id_candidato',$id_candidato)->where('tipo_arquivo','Histórico')->where('id_inscricao_verao',$id_inscricao_verao)->orderBy('created_at','desc')->first())) {
             
-            return $this->select('nome_arquivo')->where('id_candidato',$id_candidato)->where('tipo_arquivo','Histórico')->where('id_inscricao_pos',$id_inscricao_pos)->orderBy('created_at','desc')->first();
+            return $this->select('nome_arquivo')->where('id_candidato',$id_candidato)->where('tipo_arquivo','Histórico')->where('id_inscricao_verao',$id_inscricao_verao)->orderBy('created_at','desc')->first();
         }else{
 
             return $this->select('nome_arquivo')->where('id_candidato',$id_candidato)->where('tipo_arquivo','Histórico')->orderBy('created_at','desc')->first();
         }
     }
 
-    public function retorna_documento($id_candidato,$id_inscricao_pos)
+    public function retorna_documento($id_candidato,$id_inscricao_verao)
     {   
-        if (!is_null($this->select('nome_arquivo')->where('id_candidato',$id_candidato)->where('tipo_arquivo','Documentos')->where('id_inscricao_pos',$id_inscricao_pos)->orderBy('created_at','desc')->first())) {
+        if (!is_null($this->select('nome_arquivo')->where('id_candidato',$id_candidato)->where('tipo_arquivo','Documentos')->where('id_inscricao_verao',$id_inscricao_verao)->orderBy('created_at','desc')->first())) {
             
-            return $this->select('nome_arquivo')->where('id_candidato',$id_candidato)->where('tipo_arquivo','Documentos')->where('id_inscricao_pos',$id_inscricao_pos)->orderBy('created_at','desc')->first();
+            return $this->select('nome_arquivo')->where('id_candidato',$id_candidato)->where('tipo_arquivo','Documentos')->where('id_inscricao_verao',$id_inscricao_verao)->orderBy('created_at','desc')->first();
         }else{
 
             return $this->select('nome_arquivo')->where('id_candidato',$id_candidato)->where('tipo_arquivo','Documentos')->orderBy('created_at','desc')->first();
