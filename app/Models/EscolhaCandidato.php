e3677daa9502b70fa0da4bdd22013323b3c9faba
jfreitasmat <jfreitas@mat.unb.br>
2018-07-02 21:54:40 -0300

    Nome da coluna com o id do evento.

--- app/Models/EscolhaCandidato.php
+++ app/Models/EscolhaCandidato.php
@@ -18,28 +18,28 @@ class EscolhaCandidato extends Model
         'area_pos',
         'interesse_bolsa',
         'vinculo_empregaticio',
-        'id_inscricao_pos',
+        'id_inscricao_verao',
     ];
 
-    public function retorna_escolha_candidato($id_candidato,$id_inscricao_pos)
+    public function retorna_escolha_candidato($id_candidato,$id_inscricao_verao)
     {
-        return $this->where("id_candidato", $id_candidato)->where("id_inscricao_pos", $id_inscricao_pos)->get()->first();
+        return $this->where("id_candidato", $id_candidato)->where("id_inscricao_verao", $id_inscricao_verao)->get()->first();
     }
 
-    public function usuarios_nao_finalizados($id_inscricao_pos)
+    public function usuarios_nao_finalizados($id_inscricao_verao)
     {
-        return $this->where('id_inscricao_pos', $id_inscricao_pos)->join('users', 'users.id_user', 'escolhas_candidato.id_candidato')->join('programa_pos_mat', 'programa_pos_mat.id_programa_pos', 'escolhas_candidato.programa_pretendido')->whereNotIn('escolhas_candidato.id_candidato', function($query) use ($id_inscricao_pos) {
-            $query->select('id_candidato')->from('finaliza_inscricao')->where('id_inscricao_pos', $id_inscricao_pos);
+        return $this->where('id_inscricao_verao', $id_inscricao_verao)->join('users', 'users.id_user', 'escolhas_candidato.id_candidato')->join('programa_pos_mat', 'programa_pos_mat.id_programa_pos', 'escolhas_candidato.programa_pretendido')->whereNotIn('escolhas_candidato.id_candidato', function($query) use ($id_inscricao_verao) {
+            $query->select('id_candidato')->from('finaliza_inscricao')->where('id_inscricao_verao', $id_inscricao_verao);
         } )->select('users.nome', 'programa_pos_mat.tipo_programa_pos_ptbr')->orderBy('programa_pos_mat.tipo_programa_pos_ptbr');
     }
 
-    public function grava_escolhas_candidato($id_candidato,$id_inscricao_pos,$request)
+    public function grava_escolhas_candidato($id_candidato,$id_inscricao_verao,$request)
     {
 
-        $candidato_fez_escolhas = $this->retorna_escolha_candidato($id_candidato,$id_inscricao_pos);
+        $candidato_fez_escolhas = $this->retorna_escolha_candidato($id_candidato,$id_inscricao_verao);
 
         if (!is_null($candidato_fez_escolhas)) {
-            $atualiza_escolhas = $this->where('id_candidato', $id_candidato)->where('id_inscricao_pos',$id_inscricao_pos);
+            $atualiza_escolhas = $this->where('id_candidato', $id_candidato)->where('id_inscricao_verao',$id_inscricao_verao);
             $dados_escolhas['programa_pretendido'] = (int)$request->programa_pretendido;
             if ($request->programa_pretendido == 1) {
                 $dados_escolhas['area_pos'] = 10;
@@ -61,18 +61,18 @@ class EscolhaCandidato extends Model
             }
             $escolhas_candidato->interesse_bolsa = (bool)$request->interesse_bolsa;
             $escolhas_candidato->vinculo_empregaticio = (bool)$request->vinculo_empregaticio;
-            $escolhas_candidato->id_inscricao_pos = $id_inscricao_pos;
+            $escolhas_candidato->id_inscricao_verao = $id_inscricao_verao;
             $escolhas_candidato->save();
         }
     }
 
-    public function retorna_area_distintas($id_inscricao_pos)
+    public function retorna_area_distintas($id_inscricao_verao)
     {
-        return $this->select('area_pos')->where('id_inscricao_pos', $id_inscricao_pos)->where('programa_pretendido', '2')->distinct()->orderBy('area_pos')->pluck('area_pos');
+        return $this->select('area_pos')->where('id_inscricao_verao', $id_inscricao_verao)->where('programa_pretendido', '2')->distinct()->orderBy('area_pos')->pluck('area_pos');
     }
 
-    public function retorna_inscritos_por_area_pos($area_pos, $id_inscricao_pos)
+    public function retorna_inscritos_por_area_pos($area_pos, $id_inscricao_verao)
     {
-        return $this->select('id_candidato')->where('programa_pretendido', '2')->where('area_pos', $area_pos)->where('id_inscricao_pos', $id_inscricao_pos)->get()->pluck('id_candidato');
+        return $this->select('id_candidato')->where('programa_pretendido', '2')->where('area_pos', $area_pos)->where('id_inscricao_verao', $id_inscricao_verao)->get()->pluck('id_candidato');
     }
 }
