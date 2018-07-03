5c9c8a293a2d45c6b4f9dc3f2355c5f723df848f
jfreitasmat <jfreitas@mat.unb.br>
2018-07-03 14:38:02 -0300

    Exibindo os cursos para o candidato escolher.

--- resources/views/templates/partials/candidato/escolha_candidato.blade.php
+++ resources/views/templates/partials/candidato/escolha_candidato.blade.php
@@ -24,6 +24,19 @@
       </div>
   </fieldset>
 
+  @if(isset($cursos_verao))
+    <fieldset class="scheduler-border">
+      <legend class="scheduler-border">{{trans('tela_escolha_candidato.texto_cursos_verao')}}</legend>
+        <div class="row"">
+            @foreach ($cursos_verao as $curso)
+              <div class="col-md-4">
+                <label class="radio-inline">{!! Form::checkbox('curso_desejado[]', $curso->id_curso_verao, False , []) !!}{{ " ".$curso->$nome_coluna }}{{ $curso->seleciona_pos ? " (Seleciona para a Pós)" : "" }}</label>
+              </div>    
+            @endforeach
+        </div>
+    </fieldset>
+  @endif
+
   <fieldset class="scheduler-border">
   <legend class="scheduler-border"></legend>
     <div class="row">
