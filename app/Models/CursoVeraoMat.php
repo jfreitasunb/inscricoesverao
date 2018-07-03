4268390a0166170248ae483c380fcc45fba36f92
jfreitasmat <jfreitas@mat.unb.br>
2018-07-03 18:55:42 -0300

    Retorna os cursos disponíveis.

--- app/Models/CursoVeraoMat.php
+++ app/Models/CursoVeraoMat.php
@@ -27,6 +27,11 @@ class CursoVeraoMat extends Model
         'nome_es',
     ];
 
+    public function retorna_cursos_de_verao()
+    {
+        return $this->where('id_curso_verao','>', 1)->orderBy('id_curso_verao')->get();
+    }
+
     public function define_nome_coluna_por_locale($locale)
     {
         switch ($locale) {
