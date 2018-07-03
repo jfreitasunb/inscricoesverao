2c7f689695ed0d74f866e79f0f7d0a208aea1dcc
jfreitasmat <jfreitas@mat.unb.br>
2018-07-02 22:00:35 -0300

    Define o nome da coluna com base no locale.

--- app/Models/OfertaCursoVerao.php
+++ app/Models/OfertaCursoVerao.php
@@ -16,4 +16,21 @@ class OfertaCursoVerao extends Model
 
     protected $table = 'oferta_curso_verao';
 
+    public function define_nome_coluna_por_locale($locale)
+    {
+        switch ($locale) {
+            case 'en':
+                return 'nome_en';
+                break;
+
+            case 'es':
+                return 'nome_es';
+                break;
+            
+            default:
+                return 'nome_ptbr';
+                break;
+        }
+    }
+
 }
