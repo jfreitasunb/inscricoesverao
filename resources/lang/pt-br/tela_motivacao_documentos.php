9ed82a3c7dfb17101b731764286a0aa5afa7a4c9
jfreitasmat <jfreitas@mat.unb.br>
2018-07-06 14:27:42 -0300

    Melhoria no texto.

--- resources/lang/pt-br/tela_motivacao_documentos.php
+++ resources/lang/pt-br/tela_motivacao_documentos.php
@@ -10,7 +10,7 @@ return [
     'texto_motivacao' => 'Use o quadro abaixo para expressar sua motivação com o programa pretendido. Relate ainda suas atividades extra-curriculares que considera relevantes para a análise (iniciação científica, curso de verão, medalhas em competições de Matemática, etc)',
     'motivacao' => 'Motivação, expectativa e outras informações relevantes',
     'documentos_pessoais' => 'Documento de Identificação e foto (em um mesmo PDF)',
-    'historico' => 'Histórico de graduação e mestrado, quando disponível (em um mesmo PDF)',
+    'historico' => 'Histórico de graduação e/ou mestrado, quando disponível (em um mesmo PDF)',
     'concordancia_1' => 'O solicitante declara formalmente que está de acordo com as ',
     'concordancia_2' => ' e que está ciente que as decisões da Comissão de Seleção da Matemática serão irrecorríveis, admitindo-se pedido de reconsideração 
 para a própria Comissão nas hipóteses de erros procedimentais ou materiais. (declaração feita em observância aos artigos 297-299 do Código Penal Brasileiro)',
