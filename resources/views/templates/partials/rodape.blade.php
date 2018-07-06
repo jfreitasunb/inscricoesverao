bf94a1174a26568c3aa22cff823ab1e3abf97b3c
jfreitasmat <jfreitas@mat.unb.br>
2018-07-06 14:00:20 -0300

    E-mail para dúvidas sobre o verão.

--- resources/views/templates/partials/rodape.blade.php
+++ resources/views/templates/partials/rodape.blade.php
@@ -3,7 +3,7 @@
 		<div class="footer-foot">
 			<div class="container-fluid">
 				<div class="row-fluid">
-					<p>MAT/UnB - {{ date("Y") }}  - <a href="mailto:verao@mat.unb.br">{{ trans('mensagens_gerais.duvidas_pos') }}</a> - <a href="mailto:informatica@mat.unb.br">{{ trans('mensagens_gerais.problemas') }}</a></p>
+					<p>MAT/UnB - {{ date("Y") }}  - <a href="mailto:escolaverao2019@mat.unb.br">{{ trans('mensagens_gerais.duvidas_pos') }}</a> - <a href="mailto:informatica@mat.unb.br">{{ trans('mensagens_gerais.problemas') }}</a></p>
 				</div>
 			</div>
 		</div>
