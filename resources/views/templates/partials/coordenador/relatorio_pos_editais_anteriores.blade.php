6ac5cd89a8150946766cd6f0dbdc095ca8f8c8de
jfreitasmat <jfreitas@mat.unb.br>
2018-07-09 17:27:06 -0300

    Nome de variável com o id do evento.

--- resources/views/templates/partials/coordenador/relatorio_pos_editais_anteriores.blade.php
+++ resources/views/templates/partials/coordenador/relatorio_pos_editais_anteriores.blade.php
@@ -16,9 +16,9 @@
 			  	<tbody>
 		  			@foreach ($relatorios_anteriores as $relatorio)
 		  				<tr>
-				      	<th scope="row"><a href="{!! route('gera.anteriores', ['id_inscricao_pos' => $relatorio['id_inscricao_pos']]) !!}">{{$relatorio['edital']}}</a></th>
-				      	<td><a href="{!! route('gera.anteriores', ['id_inscricao_pos' => $relatorio['id_inscricao_pos']]) !!}">{{\Carbon\Carbon::parse($relatorio['inicio_inscricao'])->format('d/m/Y')." à ".\Carbon\Carbon::parse($relatorio['fim_inscricao'])->format('d/m/Y')}}</a></td>
-				      	<td>@if($monitoria == $relatorio['id_inscricao_pos'] or Session::get('monitoria') == $relatorio['id_inscricao_pos']) 
+				      	<th scope="row"><a href="{!! route('gera.anteriores', ['id_inscricao_verao' => $relatorio['id_inscricao_verao']]) !!}">{{$relatorio['edital']}}</a></th>
+				      	<td><a href="{!! route('gera.anteriores', ['id_inscricao_verao' => $relatorio['id_inscricao_verao']]) !!}">{{\Carbon\Carbon::parse($relatorio['inicio_inscricao'])->format('d/m/Y')." à ".\Carbon\Carbon::parse($relatorio['fim_inscricao'])->format('d/m/Y')}}</a></td>
+				      	<td>@if($monitoria == $relatorio['id_inscricao_verao'] or Session::get('monitoria') == $relatorio['id_inscricao_verao']) 
 				      		@if (is_array($arquivos_zipados_para_view))
 				      			@foreach ($arquivos_zipados_para_view as $zip_relatorio)
 				      		 		<a target="_blank" href="{{asset('storage/relatorios/edital_'.$relatorio['edital'].'/zip/'.$zip_relatorio)}}" >{{$zip_relatorio}}</a>
@@ -29,7 +29,7 @@
 				      			@endforeach
 				      		@endif
 				      		@endif</td>
-				      	<td>@if($monitoria == $relatorio['id_inscricao_pos']  or Session::get('monitoria') == $relatorio['id_inscricao_pos'] ) <a target="_blank" href="{{asset('storage/relatorios/edital_'.$relatorio['edital'].'/'.$relatorio_csv)}}">{{Session::has('relatorio_csv')? Session::get('relatorio_csv'): $relatorio_csv}}</a> @endif</td>
+				      	<td>@if($monitoria == $relatorio['id_inscricao_verao']  or Session::get('monitoria') == $relatorio['id_inscricao_verao'] ) <a target="_blank" href="{{asset('storage/relatorios/edital_'.$relatorio['edital'].'/'.$relatorio_csv)}}">{{Session::has('relatorio_csv')? Session::get('relatorio_csv'): $relatorio_csv}}</a> @endif</td>
 				    	</tr>
 		  			@endforeach
 			  	</tbody>
