fa874ea5f65fef9a3fda5f56e78af002e3320c25
jfreitasmat <jfreitas@mat.unb.br>
2018-07-09 17:54:23 -0300

    Coluna com o ano do evento.

--- resources/views/templates/partials/coordenador/relatorio_pos_edital_vigente.blade.php
+++ resources/views/templates/partials/coordenador/relatorio_pos_edital_vigente.blade.php
@@ -12,6 +12,7 @@
 		    <table class="table table-striped">
 			  	<thead>
 				    <tr>
+				      <th>Ano</th>
 				      <th>Período de Inscrição</th>
 				      <th>Relatório de Inscritos</th>
 				      <th>Lista de Inscritos</th>
@@ -19,6 +20,7 @@
 			  	</thead>
 			  	<tbody>
 			    	<tr>
+			    	<td><a href="{!! route('gera.relatorio', ['id_inscricao_verao' => $relatorio_disponivel['id_inscricao_verao']]) !!}">{{ $relatorio_disponivel->ano_evento }}</a></td>
 			      	<td><a href="{!! route('gera.relatorio', ['id_inscricao_verao' => $relatorio_disponivel['id_inscricao_verao']]) !!}">{{\Carbon\Carbon::parse($relatorio_disponivel['inicio_inscricao'])->format('d/m/Y')." à ".\Carbon\Carbon::parse($relatorio_disponivel['fim_inscricao'])->format('d/m/Y')}}</a></td>
 			      	<td>@if($monitoria == $relatorio_disponivel['id_inscricao_verao'] || isset($local_arquivos)) 
 			      		@foreach ($arquivos_zipados_para_view as $zip_relatorio)
